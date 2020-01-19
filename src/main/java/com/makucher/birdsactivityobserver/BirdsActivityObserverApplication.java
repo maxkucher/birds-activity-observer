2bbfecbdf53628fc12920182050821e81de58fde
Maksym Kucher <the.stewk@gmail.com>
2020-01-19 18:38:05 -0500

    repo initialized

---
+++ src/main/java/com/makucher/birdsactivityobserver/BirdsActivityObserverApplication.java
@@ -0,0 +1,13 @@
+package com.makucher.birdsactivityobserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdsActivityObserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirdsActivityObserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/makucher/birdsactivityobserver/BirdsActivityObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makucher.birdsactivityobserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdsActivityObserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
